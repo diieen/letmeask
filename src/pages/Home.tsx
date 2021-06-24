0ff6a9ba11b20712311a610be777297056f57400
Jean Ferreira <jean@prumo.digital>
2021-06-23 22:28:15 -0300

    aula 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,12 +7,18 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
+import { useAuth } from '../hooks/useAuth'
 
 export function Home() {
     const history = useHistory();
+    const { user, sigInWithGoogle } = useAuth()
 
-    function navigateToNewRoom() {
-        history.push('/rooms/new')
+    async function handleCreateRoom() {
+        if (!user) {
+            await sigInWithGoogle()
+        }
+        
+        history.push('/rooms/new') 
     }
 
     return (
@@ -25,7 +31,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask"/>
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google"/>
                         Crie sua sala com o Google
                     </button>
