746dbddf849ee5acbc666b44c4d88856751d7788
Jean Ferreira <jean@prumo.digital>
2021-06-24 23:37:58 -0300

    aula 3/2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { createContext, useState, useEffect } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { auth, firebase } from './service/firebase';
+import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext'
 
@@ -11,8 +10,11 @@ function App() {
   return (
       <BrowserRouter>
         <AuthContextProvider>
+          <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/rooms/new" component={NewRoom} /> 
+            <Route path="/rooms/:id" component={Room} /> 
+          </Switch>
         </AuthContextProvider>
       </BrowserRouter>
     );
